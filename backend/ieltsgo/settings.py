64da8981bdbb1fb75a00cbd17d98389427c30360
lloyd-axe <lloydacha@gmail.com>
2025-02-26 17:33:54 +0800

    docker postgres fix

--- backend/ieltsgo/settings.py
+++ backend/ieltsgo/settings.py
@@ -121,7 +121,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME'),
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'), 
-        'PORT': os.getenv('DB_PORT'),
+        'PORT': os.getenv('DB_PORT', '5432'),
         'HOST': os.getenv('DB_HOST'),
     }
 }
