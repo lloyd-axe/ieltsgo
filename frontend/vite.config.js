acbe7c42bf1754d0ec196158fcaf391b53150785
lloyd-axe <lloydacha@gmail.com>
2025-03-04 18:52:47 +0800

    reroute fix 2

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    historyApiFallback: true,
+  },
   base: '/static/',
   build: {
     outDir: 'dist',
