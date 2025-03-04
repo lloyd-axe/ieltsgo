cdf1cabc956a05408cdf65846a08562deb5a2c55
lloyd-axe <lloydacha@gmail.com>
2025-03-04 19:51:45 +0800

    added filter for post

--- backend/ieltsgo/urls.py
+++ backend/ieltsgo/urls.py
@@ -4,11 +4,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
 from django.urls import re_path
+from django.http import HttpResponseNotFound
+
+def serve_react_frontend(request):
+    if request.method != "GET":
+        return HttpResponseNotFound()
+    return TemplateView.as_view(template_name="index.html")(request)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("ieltsgo/", include("ielts.urls")),
-    path("", TemplateView.as_view(template_name='index.html')),
+    re_path(r"^(?!admin/|ieltsgo/).*$", serve_react_frontend),
 ]
 
 if settings.DEBUG:
